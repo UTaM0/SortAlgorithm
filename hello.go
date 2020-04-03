2bdf1611f3aa18387335b89c57c43f27a03123e4
UTaM0 <41279335+UTaM0@users.noreply.github.com>
2020-04-03 10:32:27 +0900

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	)
 
+// Sort Array
 func main() {
-	fmt.Println("Hello, World!")
+	var N int
+	fmt.Scan(&N)
+	Hi := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scan(&Hi[i])
+	}
+	sort.Ints(Hi)
+	fmt.Println(Hi)
 }
